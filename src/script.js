6b9218b75b3f8bcd91910cd632a26918b7d8d3f8
polllyann <108572062+polllyann@users.noreply.github.com>
2022-07-21 19:46:51 +0300

    JS - added day and month

---
+++ src/script.js
@@ -0,0 +1,31 @@
+let currentTime = new Date();
+let days = [
+  "Sunday",
+  "Monday",
+  "Tuesday",
+  "Wednesday",
+  "Thursday",
+  "Friday",
+  "Saturday",
+];
+let day = days[currentTime.getDay()];
+
+let months = [
+  "Jan",
+  "Feb",
+  "Mar",
+  "Apr",
+  "May",
+  "Jun",
+  "Jul",
+  "Aug",
+  "Sep",
+  "Oct",
+  "Nov",
+  "Dec",
+];
+
+let month = months[currentTime.getMonth()];
+let dayNumber = currentTime.getDate();
+let h2 = document.querySelector("h2");
+h2.innerHTML = `${day}, ${month} ${dayNumber}`;
